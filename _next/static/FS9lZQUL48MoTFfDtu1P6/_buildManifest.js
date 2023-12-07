cee0eb756f7df50521476c4b8ea8f927a7da31a2
aytbunbun <aytbunbun@users.noreply.github.com>
2023-12-07 16:29:03 +0000

    deploy: 084a4c070364efecb8debd6658e4d04bd40eee19

---
+++ _next/static/FS9lZQUL48MoTFfDtu1P6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/3d7a33e5ed73298b.css","static/chunks/pages/index-5a612e10684a2c4c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
