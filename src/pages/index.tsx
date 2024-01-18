981f5662e8debd516c47ebbf30dbe5d5ce4f81a1
aytbunbun <s1F102301000@iniad.org>
2024-01-18 20:53:12 +0900

    表示する牌をずれせるようになった

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import styles from './index.module.css';
 
 const Home = () => {
+  const number = 38; //ここの値で表示する牌をずらせる
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {[...Array(14)].map((_, i) => (
-          <div className={styles.pai} key={i} />
+          <div
+            className={styles.pai}
+            key={i}
+            style={{ backgroundPositionX: `calc(${100 * (number / 38)}%)` }}
+          />
         ))}
       </div>
     </div>
